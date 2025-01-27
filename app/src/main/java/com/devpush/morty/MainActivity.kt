7525b0613182c2703ed27ffb844f7a39e62a948c
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-01-27 15:40:37 -0800

    :alien: ktor get api set up

--- app/src/main/java/com/devpush/morty/MainActivity.kt
+++ app/src/main/java/com/devpush/morty/MainActivity.kt
@@ -4,26 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.devpush.morty.ui.theme.MortyTheme
+import com.devpush.network.KtorClient
+import com.devpush.network.models.remote.RemoteCharacter
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
+
+    private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var character: RemoteCharacter? by remember{
+                mutableStateOf(null)
+            }
+
+            LaunchedEffect(Unit) {
+                character = ktorClient.getCharacters(1)
+            }
+
+
             MortyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        Text(text = character?.name ?:"No Name Found")
+                    }
+
                 }
             }
         }
--- network/src/main/java/com/devpush/network/KtorClient.kt
+++ network/src/main/java/com/devpush/network/KtorClient.kt
@@ -1,2 +1,36 @@
 package com.devpush.network
 
+import com.devpush.network.models.domain.Character
+import com.devpush.network.models.remote.RemoteCharacter
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class KtorClient{
+    private val client = HttpClient(OkHttp) {
+        defaultRequest { url("https://rickandmortyapi.com/api/") }
+
+        install(Logging) {
+            logger = Logger.SIMPLE
+        }
+
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    suspend fun getCharacters(id:Int): RemoteCharacter {
+        return client.get("character/$id").body()
+    }
+}
